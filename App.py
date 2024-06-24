93badce6e7a1a2e551db9f8084a67f7c7c7b6610
Azurieum <mlgopnope@gmail.com>
2024-06-23 22:21:46 -0400

    messing around with frames trynna get it right:

--- App.py
+++ App.py
@@ -9,6 +9,8 @@ import subprocess
 import cpuinfo
 import threading
 
+data = open("data.txt", "r") 
+password = data.readline()
 def get_size(bytes, suffix="B"):
     factor = 1024
     for unit in ["", "K", "M", "G", "T", "P"]:
@@ -60,30 +62,37 @@ APP.resizable(False, False)
 DeviceIP, DeviceName, WifiName, CPU, UsedMemory, TotalMemory, GPU = ComputerStats()
 
 # Main labels
-label = CTkLabel(master=APP, text="COMMANDER", font=("Arial", 20))
+
+Login_Frames = CTkFrame(master=APP, fg_color="white", border_color="gray", border_width=1)
+Login_Frames.pack(expand=True)
+
+label = CTkLabel(master=Login_Frames, text="COMMANDER", font=('Helvetica', 30, 'bold'))
 label.place(anchor="nw")
 
-label1 = CTkLabel(master=APP, text=f"Username: {DeviceName}", font=("Arial", 20))
+label1 = CTkLabel(master=Login_Frames, text=f"Username: {DeviceName}", font=("Arial", 20))
 label1.place(rely=0.05, anchor="nw")
 
-label2 = CTkLabel(master=APP, text=f"Device IP: {DeviceIP}", font=("Arial", 20))
+label2 = CTkLabel(master=Login_Frames, text=f"Device IP: {DeviceIP}", font=("Arial", 20))
 label2.place(rely=0.1, anchor="nw")
 
-label3 = CTkLabel(master=APP, text=f"Wifi Name: {WifiName}", font=("Arial", 20))
-label3.place(rely=0.15, anchor="nw")
+label3 = CTkLabel(master=Login_Frames, text=f"Device IP: {DeviceIP}", font=("Arial", 20))
+label3.place(rely=0.1, anchor="nw")
 
-label4 = CTkLabel(master=APP, text=f"CPU: {CPU}", font=("Arial", 20))
-label4.place(rely=0.2, anchor="nw")
+label4 = CTkLabel(master=Login_Frames, text=f"Wifi Name: {WifiName}", font=("Arial", 20))
+label4.place(rely=0.20, anchor="nw")
 
-label9 = CTkLabel(master=APP, text=f"GPU: {GPU}", font=("Arial", 20))
-label9.place(rely=0.25, anchor="nw")
+lavel5 = CTkLabel(master=APP, text=f"CPU: {CPU}", font=("Arial", 20))
+lavel5.place(rely=0.25, anchor="nw")
+
+label6 = CTkLabel(master=APP, text=f"GPU: {GPU}", font=("Arial", 20))
+label6.place(rely=0.3, anchor="nw")
 
 # Dynamic labels for CPU and memory usage
 label_memory_usage = CTkLabel(master=APP, text=f"Memory Usage: {UsedMemory}/{TotalMemory}", font=("Arial", 20))
-label_memory_usage.place(rely=0.3, anchor="nw")
+label_memory_usage.place(rely=0.35, anchor="nw")
 
 label_cpu_usage = CTkLabel(master=APP, text="CPU Usage: 0%", font=("Arial", 20))
-label_cpu_usage.place(rely=0.35, anchor="nw")
+label_cpu_usage.place(rely=0.4, anchor="nw")
 
 # Start updating stats
 APP.after(1000, update_stats)  # Start the update loop
